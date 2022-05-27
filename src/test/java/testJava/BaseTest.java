911b795ea9fa2bba63e76b20443dd3f3c986bb04
RealGJ <34713448+RealGJ@users.noreply.github.com>
2022-05-27 10:48:04 +0530

    added basetest

--- src/test/java/testJava/BaseTest.java
+++ src/test/java/testJava/BaseTest.java
@@ -4,18 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.LoginPage;
 import utilities.ConfigUtils;
 
 public class BaseTest {
 
 	protected WebDriver driver;
+	protected LoginPage loginpage;
 	protected ConfigUtils readProp = new ConfigUtils();
 
 	@BeforeClass
@@ -27,7 +31,12 @@ public class BaseTest {
 		driver.get(readProp.getProp("url"));
 	}
 
-	@AfterSuite
+	@BeforeMethod
+	public void objectInit() {
+		loginpage = new LoginPage(driver);
+	}
+	
+	@AfterClass
 	public void envDown() {
 		driver.quit();
 	}
